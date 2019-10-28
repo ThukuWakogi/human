48b01e2ff87de368618a4ad17fac3c033ed01eba
Timothy Oliver <oliverwakogi@live.com>
2019-10-28 14:56:18 +0300

    create index view

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,23 @@
 from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_materialize import Material
+from sassutils.wsgi import SassMiddleware
 
 db = SQLAlchemy()
 
-
 def create_app(config_name):
-    app = Flask(__name__)
+  app = Flask(__name__)
+  Material(app)
 
-    app.config.from_object(config_options[config_name])
+  app.config.from_object(config_options[config_name])
 
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+  from .main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
 
-    return app
+  app.wsgi_app = SassMiddleware(app.wsgi_app, {
+    'app': ('static/sass', 'static/css', '/static/css')
+  })
+  db.init_app(app)
+
+  return app
